44ca7a7967a68a15fe966cf47ec728981790e3b8
Shah Meer <hussainshahmeer99@gmail.com>
2024-04-05 21:18:48 +0500

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  // Your webpack configuration options
+  entry: './src/ReactGithubLikeGraph.jsx', // Specify the entry point for your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory for bundled files
+    filename: 'bundle.js' // Output filename
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        use: 'raw-loader'
       }
     ]
   }
